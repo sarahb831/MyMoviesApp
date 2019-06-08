b755be00bb80535d0eb18b6e93aa47c6f2dc9805
sarahb831 <43509309+sarahb831@users.noreply.github.com>
2019-06-08 09:04:41 -0700

    change brackets

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -29553,7 +29553,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58656" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60772" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};

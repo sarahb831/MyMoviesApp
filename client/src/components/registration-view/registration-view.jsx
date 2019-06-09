d1ef679448c58e2d4b000893b19ba972078e7a5e
sarahb831 <43509309+sarahb831@users.noreply.github.com>
2019-06-09 14:24:47 -0700

    add RegistrationView details

--- client/src/components/registration-view/registration-view.jsx
+++ client/src/components/registration-view/registration-view.jsx
@@ -1,1 +1,64 @@
 //registration-view.jsx
+
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+export default function RegistrationView(props) {
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [email, setEmail ] = useState('');
+  const [ birthday, setBirthday ] = useState('');
+
+  const handleRegistration = (e) => {
+    e.preventDefault(); /* to prevent default refresh of page from this method*/
+    console.log(username, password, email, birthday);
+/* send request to server for registration */
+  };
+
+    return (
+      <Form>
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            value={username}
+            placeholder="Username"
+            onChange = {e => setUsername(e.target.value)} />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+          type="password"
+          value={password}
+          placeholder="Password"
+          onChange = {e => setPassword(e.target.value)}/>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            placeholder="Email"
+            onChange = {e => setEmail(e.target.value)}/>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicBirthday">
+          <Form.Label>Birthday (YYYY-MM-DD)</Form.Label>
+          <Form.Control
+          type="date"
+          value={birthday}
+          placeholder="Birthday"
+          onChange = {e => setBirthday(e.target.value)}/>
+        </Form.Group>
+
+        <Button
+          variant="primary"
+          type="submit"
+          onClick = {handleRegistration}>Register
+        </Button>
+      </Form>
+    );
+  }

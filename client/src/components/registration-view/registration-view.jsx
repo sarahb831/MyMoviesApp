13f87747d3bbfb2211c5121d24127890049b5e0b
sarahb831 <43509309+sarahb831@users.noreply.github.com>
2019-06-11 18:17:35 -0700

    update styling

--- client/src/components/registration-view/registration-view.jsx
+++ client/src/components/registration-view/registration-view.jsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 
 import PropTypes from 'prop-types';
@@ -19,49 +20,58 @@ export default function RegistrationView(props) {
   };
 
     return (
-      <Form>
-        <Form.Group controlId="formBasicUsername">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            value={username}
-            placeholder="Username"
-            onChange = {e => setUsername(e.target.value)} />
-        </Form.Group>
+      <Container>
+        <Row>
+          <Col></Col>
+          <Col  xs={12} md={8}>
+            <Form>
+              <Form.Group controlId="formBasicUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                value={username}
+                placeholder="Username"
+                onChange = {e => setUsername(e.target.value)} />
+              </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-          type="password"
-          value={password}
-          placeholder="Password"
-          onChange = {e => setPassword(e.target.value)}/>
-        </Form.Group>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  value={password}
+                  placeholder="Password"
+                  onChange = {e => setPassword(e.target.value)}/>
+              </Form.Group>
 
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            value={email}
-            placeholder="Email"
-            onChange = {e => setEmail(e.target.value)}/>
-        </Form.Group>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  value={email}
+                  placeholder="Email"
+                  onChange = {e => setEmail(e.target.value)}/>
+              </Form.Group>
 
-        <Form.Group controlId="formBasicBirthday">
-          <Form.Label>Birthday (YYYY-MM-DD)</Form.Label>
-          <Form.Control
-          type="date"
-          value={birthday}
-          placeholder="Birthday"
-          onChange = {e => setBirthday(e.target.value)}/>
-        </Form.Group>
+              <Form.Group controlId="formBasicBirthday">
+                <Form.Label>Birthday (YYYY-MM-DD)</Form.Label>
+                <Form.Control
+                type="date"
+                value={birthday}
+                placeholder="Birthday"
+                onChange = {e => setBirthday(e.target.value)}/>
+              </Form.Group>
 
-        <Button
-          variant="primary"
-          type="submit"
-          onClick = {handleRegistration}>Register
-        </Button>
-      </Form>
+              <Button
+                variant="primary"
+                type="submit"
+                className = "button-primary"
+                onClick = {handleRegistration}>Register
+              </Button>
+            </Form>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
     );
   }
 

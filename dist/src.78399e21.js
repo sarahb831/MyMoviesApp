38cf86a3e31024ddda29ef872c07ce127f920e73
sarahb831 <43509309+sarahb831@users.noreply.github.com>
2019-06-11 18:26:42 -0700

    build updates

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -30170,7 +30170,7 @@ function (_React$Component) {
 }(_react.default.Component);
 
 exports.default = MovieView;
-MovieCard.propTypes = {
+MovieView.propTypes = {
   movie: _propTypes.default.shape({
     Title: _propTypes.default.string,
     Description: _propTypes.default.string,
@@ -30500,7 +30500,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52530" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53962" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};

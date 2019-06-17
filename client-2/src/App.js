4dad0cc06bc84c1629fc329b7d98555df4a1b09e
SBeauchamp <smbeauchamp@mindspring.com>
2019-06-17 12:27:08 -0700

    add store, wrap app in provider

--- client-2/src/App.js
+++ client-2/src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { createStore } from 'redux';
+import { Provider } from  'react-redux';
+
+import moviesApp from './reducers/reducers';
 
 import MainView from './components/main-view/main-view';
 
 import './App.css';
 
+const store = createStore(movieApp);
+
 class App extends Component {
   render() {
-    return (
-      <MainView/>
+    return (  
+      <Provider store = {store}>
+        <MainView/>
+      </Provider>
     );
   }
 }

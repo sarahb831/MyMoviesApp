a50acad8d8c4328d3c4e02c1faa11de400ccadd3
SBeauchamp <smbeauchamp@mindspring.com>
2019-06-19 07:12:27 -0700

    add Router as parent component of Provider

--- client-2/src/App.js
+++ client-2/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { createStore } from 'redux';
 import { Provider } from  'react-redux';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import moviesApp from './reducers/reducers';
 
 import MainView from './components/main-view/main-view';
@@ -13,9 +15,11 @@ const store = createStore(moviesApp);
 class App extends Component {
   render() {
     return (  
+      <Router>
       <Provider store = {store}>
         <MainView/>
       </Provider>
+      </Router>
     );
   }
 }

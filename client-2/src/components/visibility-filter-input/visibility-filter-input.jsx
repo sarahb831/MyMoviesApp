11f87696eae3e0758c0235213bed1952338a8dd9
SBeauchamp <smbeauchamp@mindspring.com>
2019-07-05 20:36:37 -0700

    change function implementation
    change connect also

--- client-2/src/components/visibility-filter-input/visibility-filter-input.jsx
+++ client-2/src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -1,24 +1,42 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import Form from 'react-bootstrap/Form';
 
 import { setFilter } from  '../../actions/actions';
 
-function VisibilityFilterInput(props) {
+/* function VisibilityFilterInput(props) {
     return <Form.Control
-        onChange={e => props.setFilter(e.target.value)}
-        value={props.visibilityFilter}
+        onChange={e => this.props.setFilter(e.target.value)}
+        value={this.props.visibilityFilter}
         placeholder="filter"
         />;
 }
+*/
 
-export default connect(
+const mapStateToProps = state => {
+    return { currentFilter: state.visibilityFilter };
+}
+
+const VisibilityFilterInput = ({ currentFilter, setFilter }) => {
+    console.log('VFI, currentFilter: ', currentFilter);
+    return <Form.Control
+        onChange={e => setFilter(e.target.value) }
+        value = {currentFilter}
+        placeholder='Movie Title Filter'
+        />;
+
+};
+
+export default connect( mapStateToProps, { setFilter })(VisibilityFilterInput)
+/* export default connect(
     ({visibilityFilter}) => ({visibilityFilter}), { setFilter }
 )(VisibilityFilterInput);
+*/
 
-VisibilityFilterInput.propTypes = {
+/*VisibilityFilterInput.propTypes = {
     setFilter: PropTypes.func.isRequired,
     visibilityFilter: PropTypes.func.isRequired
   };
+  */

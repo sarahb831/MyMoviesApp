17f5e2ba316d6a584b79eefc815f1eaa70a66813
SBeauchamp <smbeauchamp@mindspring.com>
2019-07-08 23:43:35 -0700

    remove console.logs, neaten code

--- client-2/src/components/director-view/director-view.jsx
+++ client-2/src/components/director-view/director-view.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import { Link } from  'react-router-dom';
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -8,61 +10,51 @@ import Card from 'react-bootstrap/Card';
 
 import './director-view.scss';
 
-import { Link } from  'react-router-dom';
-
 export default class DirectorView extends React.Component {
 
   constructor() {
     super();
-
     this.state = {};
   }
 
   render() {
-    const { movie } = this.props;
+    const { director } = this.props;
 
-    console.log('dview, movie:', movie ? movie : 'no movie found');
-    console.log('dview, director:', movie ? movie.Director : 'no director found');
-    if (!movie) return null;
-    if (movie && movie.Director ) {
+    if (!director) return null;
     return (
       <Container>
-      <Row>
-        <Col></Col>
-        <Col xs={12} md={8}>
+        <Row>
+        <Col md={3}></Col>
+        <Col xs={12} sm={6}>
           <Card className = "director-view" style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title className = "director-name">Director: {movie.Director.Name}</Card.Title>
-          <Card.Text className = "director-bio">Biography: {movie.Director.Bio}
-          </Card.Text>
-          <Card.Text className = "director-birth">Birth: {movie.Director.Birth}</Card.Text>
-          <Card.Text className = "director-death">Death: {movie.Director.Death}</Card.Text>
-        </Card.Body>
+            <Card.Body>
+              <Card.Title className = "director-name">Director: {director.Name}</Card.Title>
+              <Card.Text className = "director-bio">Biography: {director.Bio}</Card.Text>
+              <Card.Text className = "director-birth">Birth: {director.Birth}</Card.Text>
+              {director.Death !== "0" && 
+                <Card.Text className = "director-death">Death: {director.Death}</Card.Text>}
+            </Card.Body>
         
-        <Card.Body>
-        <Link to={`/`}>
-            <Button variant="link" className = "button-primary">
-              Return to Main Menu 
-            </Button>
-          </Link>
-        </Card.Body>
-      </Card>
-      </Col>
+            <Card.Body>
+              <Link to={`/`}>
+                <Button variant="link" className = "button-primary" block>
+                  Return to Main Menu 
+                </Button>
+              </Link>
+            </Card.Body>
+          </Card>
+        </Col>
         <Col>
         </Col>
       </Row>
     </Container>
     );
-    } else return null;
   }
 }
 
-/*DirectorView.propTypes = {
+DirectorView.propTypes = {
   director: PropTypes.shape({
     Name: PropTypes.string,
-    Bio: PropTypes.string,
-    Birth: PropTypes.string,
-    Death: PropTypes.string
+    Bio: PropTypes.string
   }).isRequired
-};*/
-
+};

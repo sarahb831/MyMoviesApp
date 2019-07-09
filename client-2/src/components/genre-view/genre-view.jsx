6f7c75a32ac890c87fcfda7f013f97802268284d
SBeauchamp <smbeauchamp@mindspring.com>
2019-07-08 23:44:15 -0700

    remove console.logs, neaten code

--- client-2/src/components/genre-view/genre-view.jsx
+++ client-2/src/components/genre-view/genre-view.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from  'react-router-dom';
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
-import { Link } from  'react-router-dom';
 
 import './genre-view.scss';
 
@@ -14,44 +14,38 @@ export default class GenreView extends React.Component {
 
   constructor() {
     super();
-
     this.state = {};
   }
 
   render() {
-    const { movie } = this.props;
-    console.log('gview, movie:', movie ? movie : 'no movie found');
-    console.log('gview, director:', movie ? movie.Genre.Name : 'no genre found');
-    if (!movie) return null;
-
-    const genre = movie.Genre;
-
+    const { genre } = this.props;
+    if (!genre) return null;
 
     return (
       <Container>
-      <Row>
-        <Col></Col>
-        <Col xs={12} md={8}>
-          <Card className = "genre-view" style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title className = "genre-name">Genre: {genre.Name}</Card.Title>
-          <Card.Text className = "genre-description">{genre.Description}
-          </Card.Text>
-        </Card.Body>
+        <Row>
+          <Col md={3}></Col>
+          <Col xs={12} sm={6}>
+            <Card className = "genre-view" style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title className = "genre-name">Genre: {genre.Name}</Card.Title>
+                <Card.Text className = "genre-description">{genre.Description}
+                </Card.Text>
+              </Card.Body>
         
-        <Card.Body>
-        <Link to={`/`}>
-            <Button variant="link" className = "button-primary">
-              Return to Main Menu 
-            </Button>
-          </Link>
-        </Card.Body>
-      </Card>
-      </Col>
-        <Col>
-        </Col>
-      </Row>
-    </Container>
+              <Card.Body>
+                <Link to={`/`}>
+                  <Button variant="link" className = "button-primary" block>
+                    Return to Main Menu 
+                  </Button>
+                </Link>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
